bac9c1d9046216a1abc461a2ab01f4d129b09f05
Lester <lester.ung@pathmazing.com>
2017-06-23 21:09:46 +0700

    update

--- src/app/chat.service.ts
+++ src/app/chat.service.ts
@@ -21,7 +21,7 @@ export class ChatService {
 
   saveChat(data) {
     return new Promise((resolve, reject) => {
-      this.http.post('/cha/', data)
+      this.http.post('/chat/', data)
         .map(res => res.json())
         .subscribe(res => {
           resolve(res);
